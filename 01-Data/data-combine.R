a43b976cc501a077e1155ec2858a186d7c1f2836
hennip <jeannie.jne@gmail.com>
2018-12-05 12:31:53 +0200

    Full juvenile data

--- 01-Data/data-combine.R
+++ 01-Data/data-combine.R
@@ -1,33 +1,51 @@
 
-dfT<-full_join(df_MS,dfU, by=NULL)%>%
+df<-full_join(df_MS,dfU, by=NULL)%>%
   full_join(df_Inari, by=NULL)%>%
   full_join(df_trib, by=NULL)%>%
   mutate(n=ifelse(n==0, 1,n))%>%
-  filter(year>=1980)
+  filter(year>=1980)%>%
+  mutate(rcode=ifelse(rivername=="TenoMS", "01", 
+                      ifelse(rivername=="Inari","02", 
+                             ifelse(rivername=="Utsjoki", "01.03",as.character(river2)))))
 
-filter(dfT, is.na(rivername==T))
+smolts<-df%>%
+  select(rivername, rcode, year, CIS, IS)%>%
+  filter(is.na(IS)==F)
 
-RiverNames=c("TenoMS", "Pulmanki", "Vetsijoki", "Utsjoki", "Tsars", "Kevo",
-             "Kuoppilas", "Nilijoki", "Akujoki", "Inari")#, "Torne")
-
-# If Tornionjoki needs to be included
-# source("01-Data/data-baltic-north.r")
-#dfB2<-filter(dfB, stock==1) # Only Tornionjoki
-#dfB2<-dfB2%>%
-  #mutate(stock=7)
-#  mutate(rivername="Torne")
-df<-dfT%>%#full_join(dfT,dfB2, by=NULL)%>%
-  select(year, rivername, IS, CIS, n, IP0, IP1, IP2)%>%
-  mutate(stock=parse_factor(rivername, levels=RiverNames))%>%
-  mutate(stock=fct_recode(stock, "1"="TenoMS","2"="Pulmanki", "3"="Vetsijoki", 
-                          "4"="Utsjoki","5"="Tsars", "6"="Kevo", 
-                          "7"="Kuoppilas","8"="Nilijoki", "9"="Akujoki", 
-                          "10"="Inari"))#, "7"="Torne"))
-
-#View(df_MS)
+dfT<-df%>%
+  group_by(rcode, rivername, year)%>%
+  summarise(IP0=mean(IP0, na.rm=T),IP1=mean(IP1, na.rm=T),IP2=mean(IP2, na.rm=T), n=sum(n))%>%
+  ungroup()%>%
+  mutate(IP0=ifelse(is.na(IP0)==T,NA_real_,IP0))%>%
+  mutate(IP1=ifelse(is.na(IP1)==T,NA_real_,IP1))%>%
+  mutate(IP2=ifelse(is.na(IP2)==T,NA_real_,IP2))
 
 
-df<-df%>%ungroup
+dfT2<-full_join(dfT, smolts)%>%
+  mutate(n=ifelse(is.na(n)==T,1,n))
+  
+
+df<-dfT2%>%
+  select(rcode,rivername, year, IS, CIS, n, IP0, IP1, IP2)%>%
+  mutate(stock=as.factor(rivername))%>%
+  mutate(stock=fct_recode(stock, "1"="TenoMS","2"="Pulmanki", 
+                          "3"="Kalddas","4"="Vetsijoki", 
+                          "5"="Utsjoki","6"="Tsars", "7"="Kevo", 
+                          "8"="Kuoppilas","9"="Nilijoki", "10"="Akujoki", 
+                          
+                          "11"="Levajoki","12"="Baisjoki", "13"="Laksejoki", 
+                          "14"="Deavkke","15"="Gurte", "16"="Valjoki", 
+                          "17"="Inari","18"="Karigas", "19"="Vuomajoki", 
+                          
+                          "20"="Kietsima","21"="Gossjohka", "22"="Karasjoki", 
+                          "23"="Iesjoki","24"="Geaime", "25"="Bavtta", 
+                          "26"="Maske","27"="Ciiko"))%>%
+  mutate(stock=as.numeric(as.character(stock)))
+
+
+
+
+#df<-df%>%ungroup
 
 n<-as.matrix(df%>%select(n, stock, year)%>%
   spread(key=stock, value=n)%>%
@@ -36,12 +54,12 @@ n<-as.matrix(df%>%select(n, stock, year)%>%
 
 CIS<-as.matrix(df%>%select(CIS, stock, year)%>%
   spread(key=stock, value=CIS)%>%
-    select(`2`,`4`,`5`,`6`,-year)%>%
+    select(`2`,`3`,`5`,`6`,`7`,`8`,`18`,-year)%>%
     replace(is.na(.),0.2))
 
 IS<-as.matrix(df%>%select(IS, stock, year)%>%
   spread(key=stock, value=IS)%>%
-    select(`2`,`4`,`5`,`6`,-year))
+    select(`2`,`3`,`5`,`6`,`7`,`8`,`18`,-year))
     
 IP1<-round(as.matrix(df%>%select(IP1, stock, year)%>%
   spread(key=stock, value=IP1)%>%
--- 01-Data/data-teno-tributaries.R
+++ 01-Data/data-teno-tributaries.R
@@ -221,58 +221,11 @@ df5<-df4%>%
   filter(is.na(area)==F)%>%
   select(river2, rivername, year, everything())
 # View(df5)
+# yht 27 streams
 
 
-
-df_trib_old<-df4%>%select(-IOP1)%>%
-  select(-river)
-
-
-# Tributary electrofishing data from 2000 and onwards
-###########################################
-
-File<-"H:/Projects/ISAMA/data/orig/TENO SAHKODATA/Sivujoki 2000-luku originaalidata 250315.xlsx"
-# Original location: G:\ex-RKTL\0_HOT\Kalavarat\Tenojoki\TENO-NÄÄTÄMÖ_SÄHKÖDATA\Sivujoet
-
-df<-read_xlsx(File, na="", 
-              range="A1:R26892",col_types=c(rep("guess",3), "date", rep("numeric",7), rep("guess",7)))
-
-df<-df%>%select(river, place, chdate, ala, maxk, k, species, age)%>%
-  mutate(year=year(chdate),month=month(chdate),day=day(chdate))%>%
-  filter(species==1)
-
-df2<-df%>%group_by(river, year, place)%>%
-  summarise(`0+`=sum(age==0),`1+`=sum(age==1),
-            `1++`=sum(age>0),`2++`=sum(age>1), # age=10 means >=2+
-            acre=mean(ala)/100)%>%
-  mutate(IP0=`0+`/acre, IP1=`1+`/acre,
-         IOP1=`1++`/acre, IP2=`2++`/acre)
-
-df2<-df2%>%select(river,year, place, acre, IP0, IP1, IP2, IOP1)%>%
-  group_by(river,year)%>%
-  summarise(IP0=mean(IP0), IP1=mean(IP1), IOP1=mean(IOP1), IP2=mean(IP2), area=mean(acre), n=n())%>%
-  #filter(n>1)
-
-#View(df2)
-
-# Pulmanki, Vetsijoki, Tsars, Kevo, 
-# Kuoppilasjoki, Nilijoki, Akujoki#, Karasjoki, Iesjoki
-df3<-df2%>%filter(river=="01.01" |river=="01.02" | river=="01.03.01" | river=="01.03.02" | 
-                    river=="01.04"|river=="01.06"|river=="01.07")#| river=="03" | river=="03.01")
-#df3<-df2
-# Name 'em
-df4<-df3%>%#filter(river=="01.01" | river=="01.03.01" | river=="01.03.02")%>%
-  ungroup()%>%
-  mutate(river=parse_factor(river, levels=NULL))%>%
-  mutate(rivername=fct_recode(river,
-                              "Pulmanki"="01.01", "Vetsijoki" ="01.02",
-                              "Tsars"="01.03.01", "Kevo" ="01.03.02",
-                              "Kuoppilas"="01.04", "Nilijoki" ="01.06",
-                              "Akujoki"="01.07"))
-  
-
-df_trib_new<-df4%>%select(-IOP1)%>%
-  select(-river)
+df_trib<-df5%>%
+  select(-area, -IOP1)
 
 
 # Smolt trap data 1989-1994: Pulmanki, Tsars, Kevo (Karigas, Kuoppilas, Kalddas not included yet)
@@ -280,16 +233,16 @@ df_trib_new<-df4%>%select(-IOP1)%>%
 
 File<-"H:/Projects/ISAMA/data/der/Teno_smolts_89-94.xlsx"
 
-df<-read_xlsx(File, na="", range="J4:P21")%>%
+df<-read_xlsx(File, na="", range="J4:P29")%>%
   mutate(mu=round((p_min+p_mean+p_max)/3,2), # transform from triangular into lognormal
          var= round((p_min^2+p_mean^2+p_max^2-p_min*p_max-p_min*p_mean-p_mean*p_max)/18,4),
          cv=round(sqrt(var)/mu,2))
 
 # JAGS model for trap data (total N = N observed / expert evaluated prop observed)
-
+# 
 # M_traps<-"model{
 # 
-# for(i in 1:17){
+# for(i in 1:25){
 #   smoltEst[i]<-n[i]/p[i]
 #   tau[i]<-1/log(cv[i]*cv[i]+1)
 #   p[i]~dlnorm(log(mu[i])-0.5/tau[i], tau[i])
@@ -301,10 +254,10 @@ df<-read_xlsx(File, na="", range="J4:P21")%>%
 # 
 # var_names<-c("smoltEst", "p")
 # 
-# run <- run.jags(M_traps, 
+# run <- run.jags(M_traps,
 #                  data=data,monitor=var_names, inits=NA,
-#                  n.chains = 2, method = 'parallel', thin=1, burnin =100, 
-#                  modules = "mix",keep.jags.files=T,sample =10000, adapt = 1000, 
+#                  n.chains = 2, method = 'parallel', thin=1, burnin =100,
+#                  modules = "mix",keep.jags.files=T,sample =10000, adapt = 1000,
 #                  progress.bar=TRUE)
 # 
 # #plot(run)
@@ -314,19 +267,25 @@ df<-read_xlsx(File, na="", range="J4:P21")%>%
 
 load("01-Data/summary_traps.RData")
 
-tmp<-as.tibble(traps[1:17,4:5])%>%
-  mutate(year=c(1989:1994, 1989:1994, 1989:1991,1993,1994),
-                 river=c(rep("Pulmanki", 6), rep("Tsars",6), rep("Kevo",5)))
+tmp<-as.tibble(traps[1:25,4:5])%>%
+  mutate(year=c(1989:1994, 1989:1994, 1989:1991,1993:1994,1990,1990:1994,1991:1992),
+                 river=c(rep("Pulmanki", 6), rep("Tsars",6), rep("Kevo",5),
+                         "Kalddas",rep("Karigas", 5),rep("Kuoppilas", 2)))
 
-tmp2<-full_join(df,tmp)%>%
+smolts_trib<-full_join(df,tmp)%>%
   select(year, river, n, Mean, SD)%>%
   mutate(CIS=round(SD/Mean,2),
     IS=log(Mean/1000*(CIS*CIS+1)))%>%
   mutate(rivername=parse_factor(river, levels=NULL))%>%
-  select(year, rivername, IS, CIS)
+  select(year, rivername, IS, CIS)%>%
+  mutate(river2=ifelse(rivername=="Pulmanki", "01.01", 
+                      ifelse(rivername=="Tsars", "01.03.01", 
+                             ifelse(rivername=="Kevo", "01.03.02", 
+                                    ifelse(rivername=="Kalddas", "01.01.01",
+                                           ifelse(rivername=="Karigas", "02.01",
+                                                  ifelse(rivername=="Kuoppilas", "01.04",NA)))))))
 
-df_trib<-full_join(df_trib_new,df_trib_old)%>%
-  full_join(tmp2)
-#View(df_trib)
+df_trib<-full_join(df_trib,smolts_trib)
+#View(tmpX)
 
 #View(df_trib%>%arrange(rivername))
